c5ffbe2d845d067009ed3fd10031f62e27894b8c
Priyansh Kedia <kediapriyansh@gmail.com>
2020-08-01 21:47:49 +0530

    customised user register forms

--- site_users/forms.py
+++ site_users/forms.py
@@ -6,8 +6,8 @@ class CreateUserForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
             super(CreateUserForm,self).__init__(*args,**kwargs)
-            self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'})
-            self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password'})
+           # self.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'})
+           # self.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password'})
             # self.fields['password1'].widget = forms.PasswordInput(attrs={'placeholder':'Password'})
             # self.fields['password2'].widget = forms.PasswordInput(attrs={'placeholder':'Confirm Password'})
             # self.fields['email'].widget = forms.PasswordInput(attrs={'placeholder':'Email'})
@@ -21,9 +21,9 @@ class CreateUserForm(UserCreationForm):
             'password1',
             'password2'
         ]
-        widgets = {
-            'email' : forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}),
-            'username' : forms.TextInput(attrs={'class':'form-control','placeholder':'Username'})
-        }
+        # widgets = {
+        #     'email' : forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}),
+        #     'username' : forms.TextInput(attrs={'class':'form-control','placeholder':'Username'})
+        # }
 
         
