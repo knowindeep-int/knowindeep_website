e5b05513a98150010bc448d2c5f3604fee405986
Priyansh Kedia <kediapriyansh@gmail.com>
2020-08-01 13:25:58 +0530

    added tinemce

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from tinymce import HTMLField
 from django.db.models.signals import pre_save,post_save
 from .utils import unique_slug_generator
 from django.db.models.signals import post_save, pre_save
@@ -8,7 +9,6 @@ class Blog(models.Model):
    # id = models.IntegerField(primary_key=True)
     topic = models.CharField(max_length=30)
     slug = models.SlugField(null=True,blank=True)
-
 
     def __str__(self):
         return self.topic
