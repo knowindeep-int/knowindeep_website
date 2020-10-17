5987dfff561ff68b90e9413bac6eb72ce42609dc
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-10-18 03:06:44 +0530

    added social auth profile

--- blogs/models.py
+++ blogs/models.py
@@ -17,13 +17,13 @@ class Profile(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200)
     email_id = models.EmailField(max_length=30)
-    phone_number = models.IntegerField(unique=True, validators=[MaxValueValidator(9999999999), MinValueValidator(1000000000)])
+    phone_number = models.IntegerField(unique=True,null=True, blank=True, validators=[MaxValueValidator(9999999999), MinValueValidator(1000000000)])
     linkedin_id = models.URLField(max_length=70,null=True,blank=True)
     github_id = models.URLField(max_length=70,null=True,blank=True)
     twitter_id = models.URLField(max_length=70, null=True, blank=True)
     isAuthor = models.BooleanField(default=False)
     account_number = models.CharField(max_length=30, null=True, blank=True)
-    total_earnings = models.IntegerField()
+    total_earnings = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
