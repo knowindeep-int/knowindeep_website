24cfab885c065ac93afec608f33f526d84fc624b
Krishang <krishangsaharia2@gmail.com>
2020-11-26 00:37:10 +0530

    commented try block

--- author/views.py
+++ author/views.py
@@ -6,14 +6,14 @@ from django.forms.models import model_to_dict
 
 def author_page(request, slug):
     context = None
-    try:
-        profile = Profile.objects.get(name=slug)
-    except Profile.DoesNotExist:
-        profile = None
-    is_verified = request.user == profile.user
-    context = {
-        "profile": profile,
-        "is_verified":is_verified
-    }
+    #try:
+    #    profile = Profile.objects.get(name=slug)
+    #except Profile.DoesNotExist:
+    #    profile = None
+    #is_verified = request.user == profile.user
+    #context = {
+    #    "profile": profile,
+    #    "is_verified":is_verified
+    #}
     return render(request,"author/author_page.html",context)
  
