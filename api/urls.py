67baa4d7439bb13ccd8c3946d5e8c88462629de4
Krishang <krishangsaharia2@gmail.com>
2020-11-27 16:13:29 +0530

    changed url from blogs/ to chapters/ for api_all_detail_view

--- api/urls.py
+++ api/urls.py
@@ -7,7 +7,7 @@ app_name = "api"
 
 urlpatterns = [
     path('update_profile/', update_profile, name='update_profile'),
-    path('blogs/',api_all_detail_view),
+    path('chapters/',api_all_detail_view),
     path('increaseView',increase_post_view,name='increase_view'),
     path('comments/',fetchComments,name='chapterComments'),
     path('<slug:slug>/',api_detail_chapter_view),
