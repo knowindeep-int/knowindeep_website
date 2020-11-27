9993ada717266924e11c5058297e590f4f28d12a
Krishang <krishangsaharia2@gmail.com>
2020-11-27 16:00:58 +0530

    changed name of api_like/comment_blog_view to api_like/comment_chapter_view

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import api_detail_blog_view, api_detail_blog_update_view, api_all_detail_view, api_like_blog_view, api_comment_blog_view, fetchComments, increase_post_view, update_profile
+from .views import api_detail_blog_view, api_detail_blog_update_view, api_all_detail_view, api_like_chapter_view, api_comment_chapter_view, fetchComments, increase_post_view, update_profile
 
 
 app_name = "api"
@@ -12,6 +12,6 @@ urlpatterns = [
     path('comments/',fetchComments,name='blogComments'),
     path('<slug:slug>/',api_detail_blog_view),
     path('<slug:slug>/update',api_detail_blog_update_view),
-    path('blogtopic/like',api_like_blog_view,name='like-post'),
-    path('blogtopic/comment',api_comment_blog_view,name="comment-post"),
+    path('chaptertopic/like',api_like_chapter_view,name='like-post'),
+    path('chaptertopic/comment',api_comment_chapter_view,name="comment-post"),
 ]
