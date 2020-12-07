0a20f2c63140b33ad81b21b24198abe95a81c7a3
Krishang <krishangsaharia2@gmail.com>
2020-12-07 10:18:59 +0530

    added languages list in context

--- teach/views.py
+++ teach/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blogs.models import Language
+
 def add_course(request):
     if request.user.is_authenticated:
-        return render(request, 'teach/new_course.html', context={})
+        languages = Language.getAllLanguages()
+        context = {
+            'languages': languages
+        }
+        return render(request, 'teach/new_course.html', context=context)
     
     raise Exception("You are not Authorized to access this page")
     #return HttpResponse("You are not Authorized to access this page", status = 500)
