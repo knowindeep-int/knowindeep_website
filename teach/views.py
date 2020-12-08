7c279d520111b1de9ace0e6495208f4c26dc56a9
Krishang <krishangsaharia2@gmail.com>
2020-12-08 13:29:15 +0530

    added pre_reqs in context

--- teach/views.py
+++ teach/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from blogs.models import Language
+from blogs.models import Language, PreRequisite
 
 def add_course(request):
     if request.user.is_authenticated:
         languages = Language.getAllLanguages()
+        pre_reqs = PreRequisite.getAllPreReqs()
         context = {
-            'languages': languages
+            'languages' : languages,
+            'pre_reqs' : pre_reqs
         }
         return render(request, 'teach/new_course.html', context=context)
     
