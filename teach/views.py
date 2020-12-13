fb8073bcad9989026e14a525d63d838d8bfe7571
Priyansh-Kedia <kediapriyansh@gmail.com>
2020-12-14 00:18:50 +0530

    minor changes and refactoring

--- teach/views.py
+++ teach/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from blogs.models import Language, PreRequisite
@@ -13,6 +13,4 @@ def add_course(request):
         }
         return render(request, 'teach/new_course.html', context=context)
     
-    raise Exception("You are not Authorized to access this page")
-    #return HttpResponse("You are not Authorized to access this page", status = 500)
-# Create your views here.
+    return redirect("/")
