9b57e5a652ae04a939694ca72bed9097a1c8d336
Krishang <krishangsaharia2@gmail.com>
2020-12-13 21:42:59 +0530

    added saveDraft url

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import api_detail_chapter_view, api_detail_chapter_update_view, api_all_detail_view, api_like_chapter_view, api_comment_chapter_view, fetchComments, increase_post_view, update_profile, search_project
+from .views import api_detail_chapter_view, api_detail_chapter_update_view, api_all_detail_view, api_like_chapter_view, api_comment_chapter_view, fetchComments, increase_post_view, update_profile, search_project, api_save_draft
 
 
 app_name = "api"
@@ -15,4 +15,5 @@ urlpatterns = [
     path('<slug:slug>/update',api_detail_chapter_update_view),
     path('chaptertopic/like',api_like_chapter_view,name='like-post'),
     path('chaptertopic/comment',api_comment_chapter_view,name="comment-post"),
+    path('save_draft', api_save_draft, name="save-draft"),
 ]
