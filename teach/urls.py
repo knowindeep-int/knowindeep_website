dbbbe6ee3b0d5d8ed37d5f7d6cd8c1f224fdb22c
Krishang <krishangsaharia2@gmail.com>
2020-12-16 15:27:50 +0530

    added url to get list of projects

--- teach/urls.py
+++ teach/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import add_course
+from .views import add_course, list_of_projects
 
 app_name = "teach"
 
 urlpatterns=[
-    path('', add_course, name="add_course")
+    path('add/', add_course, name="add_course"),
+    path('', list_of_projects, name="list_of_projects")
 ]
