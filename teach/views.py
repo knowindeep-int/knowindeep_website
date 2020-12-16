45a3906907f55c6e9338b397093d7bab488d2b53
Krishang <krishangsaharia2@gmail.com>
2020-12-16 15:28:44 +0530

    added list_of_projects view to return dummy ongoing projects in context

--- teach/views.py
+++ teach/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from blogs.models import Language, PreRequisite
+from blogs.models import Language, PreRequisite, Project
 
 def add_course(request):
     if request.user.is_authenticated:
@@ -14,3 +14,13 @@ def add_course(request):
         return render(request, 'teach/new_course.html', context=context)
     print("45")
     return redirect("/")
+
+
+def list_of_projects(request):
+    ongoing_projects = Project.objects.all()[:10]
+
+    context = {
+        'ongoing_projects': ongoing_projects
+    }
+
+    return render(request, 'teach/projects_list.html', context=context)
