6c4cdf23eaf32dee8b1e197265957db271d4d440
Krishang <krishangsaharia2@gmail.com>
2020-12-21 01:27:48 +0530

    added url for add_course with pk as parameter

--- teach/urls.py
+++ teach/urls.py
@@ -6,5 +6,6 @@ app_name = "teach"
 
 urlpatterns=[
     path('add/', add_course, name="add_course"),
+    path('add/<int:pk>/', add_course, name="add_course"),
     path('', list_of_projects, name="list_of_projects")
 ]
