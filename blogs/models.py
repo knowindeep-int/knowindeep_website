8cb81f3942a1523f4314c058bbacf367e7944c48
Krishang <krishangsaharia2@gmail.com>
2020-12-28 01:31:59 +0530

    refactored code in all models

--- blogs/models.py
+++ blogs/models.py
@@ -15,9 +15,9 @@ from django.conf import settings
 from knowindeep import Constants
 
 class Language(models.Model):
-    name = models.CharField(max_length = 100,unique = True, primary_key = True)
     is_available = models.BooleanField(null = True, blank = True)
-
+    name = models.CharField(max_length = 100,unique = True, primary_key = True)
+    
     def __str__(self):
         return self.name
 
@@ -30,28 +30,45 @@ class Language(models.Model):
         return languages
 
 class Profile(models.Model):
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    dp = models.ImageField(null=True,upload_to='profiles/', blank = True)
-    description = models.CharField(max_length=200, null = True, blank = True)
-    #email_id = models.EmailField(max_length=30, unique=True, primary_key=True)
-    phone_number = models.IntegerField(null=True, blank=True, validators=[MaxValueValidator(9999999999), MinValueValidator(1000000000)])
-    linkedin_id = models.URLField(max_length=70,null=True,blank=True)
-    github_id = models.URLField(max_length=70,null=True,blank=True)
-    twitter_id = models.URLField(max_length=70, null=True, blank=True)
-    isAuthor = models.BooleanField(default=False)
     account_number = models.CharField(max_length=30, null=True, blank=True)
-    total_earnings = models.IntegerField(null=True, blank=True)
+    description = models.CharField(max_length=200, null = True, blank = True)
+    dp = models.ImageField(null=True,upload_to='profiles/', blank = True)
+    github_id = models.URLField(max_length=70,null=True,blank=True)
+    isAuthor = models.BooleanField(default=False)
+    linkedin_id = models.URLField(max_length=70,null=True,blank=True)
+    phone_number = models.IntegerField(null=True, blank=True, validators=[MaxValueValidator(9999999999), MinValueValidator(1000000000)])
     skills = models.ManyToManyField(to = Language, related_name="skills", blank = True)
+    total_earnings = models.IntegerField(null=True, blank=True)
+    twitter_id = models.URLField(max_length=70, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    #email_id = models.EmailField(max_length=30, unique=True, primary_key=True)
     #projects = models.ManyToManyField(to = 'Project', null = True, blank = True)
 
     def __str__(self):
         return self.user.first_name + " " + self.user.last_name
 
-    def is_verified(self, user):
-        if user == self.user:
-            return True
-        return False
+    @classmethod
+    def getAuthorSearches(cls, search_input):
+        author_searches = cls.objects.filter(
+            Q(user__username__icontains = search_input)
+        )
+        return author_searches
+    
+    @classmethod
+    def getProfile(cls,user):
+        return cls.objects.get(user=user)
+
+    @classmethod
+    def getUser(cls, name):
+        profile = cls(name = name)
+        return profile.user
+
+    @property
+    def getOngoingAndCompletedProjects(self):
+        projects = self.projects.all()
+        ongoing_projects = projects.filter(isCompleted=False)
+        completed_projects = projects.filter(isCompleted=True)
+        return ongoing_projects, completed_projects
 
     @property
     def getPackageProgressTuple(self):
@@ -62,40 +79,24 @@ class Profile(models.Model):
             percent_progress.append(packages[i].progress.all().count() / packages[i].project.chapters.all().count() * 100)        
         
         return tuple(zip(packages, percent_progress))
-
-    @classmethod
-    def getUser(cls, name):
-        profile = cls(name = name)
-        return profile.user
-
-    @classmethod
-    def getProfile(cls,user):
-        return cls.objects.get(user=user)
-        
-    @property
-    def getOngoingAndCompletedProjects(self):
-        projects = self.projects.all()
-        ongoing_projects = projects.filter(isCompleted=False)
-        completed_projects = projects.filter(isCompleted=True)
-        return ongoing_projects, completed_projects
-
+    
     @property
     def skills_set(self):
         return self.skills.all()
+    
 
-    @classmethod
-    def getAuthorSearches(cls, search_input):
-        author_searches = cls.objects.filter(
-            Q(user__username__icontains = search_input)
-        )
-        return author_searches
+    def is_verified(self, user):
+        if user == self.user:
+            return True
+        return False
+
+    
 
 class Blog(models.Model):
-    profile = models.ForeignKey(to =Profile, on_delete = models.CASCADE)
     content = RichTextUploadingField(blank=True,null=True)
+    profile = models.ForeignKey(to =Profile, on_delete = models.CASCADE)
     title = models.CharField(max_length = 25, null =False, blank= False)
-
-
+    
 
 class PreRequisite(models.Model):
     name = models.CharField(max_length = 100)
@@ -111,34 +112,36 @@ class PreRequisite(models.Model):
         pre_reqs = cls.objects.all()
         return pre_reqs
 
+
 class Project(models.Model):
      # topic = models.CharField(max_length=30)
     # topic_image = models.ImageField(null=True, upload_to='media/')
     # topic_content = models.CharField(max_length=300, blank=True, null=True)
-    slug = models.SlugField(null=True,blank=True)
-    no_of_views = models.IntegerField(default=0)
-    description = models.TextField(null = True, blank = True)
     author = models.ForeignKey(to = Profile, on_delete = models.CASCADE, related_name = "projects")
-    no_of_hours = models.DecimalField(null = True, blank = True, decimal_places = 1, max_digits = 4)
+    description = models.TextField(null = True, blank = True)
     difficulty_level = models.CharField(max_length = 100,null = True, blank = True, choices = ((Constants.EASY, Constants.EASY),(Constants.MEDIUM, Constants.MEDIUM), (Constants.HARD, Constants.HARD)))
     isApproved = models.BooleanField(default=False)
-    languages = models.ManyToManyField(to = Language, blank = True)
-    # update
+    isCompleted = models.BooleanField(default = False)
     image = models.ImageField(null=True,upload_to='project/', blank = True)
-    title = models.CharField(max_length=25)
+    languages = models.ManyToManyField(to = Language, blank = True)
+    no_of_hours = models.DecimalField(null = True, blank = True, decimal_places = 1, max_digits = 4)
+    no_of_views = models.IntegerField(default=0)
     overview = models.CharField(max_length=300, null = True, blank = True)
     pre_req = models.ManyToManyField(to = PreRequisite, blank = True)
-    isCompleted = models.BooleanField(default = False)
-
+    slug = models.SlugField(null=True,blank=True)
+    title = models.CharField(max_length=25)    
+    
     def __str__(self):
         return self.title
     
     @property
+    def complete_project(self):
+        self.isCompleted = True
+        self.save()
+
+    @property
     def get_absolute_url(self): 
         return reverse('blogs:sub_topic', kwargs={'slug': self.slug})
-
-    def canUserView(self, user):
-        return self.isApproved or user.is_superuser
 
     @property
     def getCompleteUrl(self):
@@ -147,10 +150,45 @@ class Project(models.Model):
         else:
             return "https://%s%s" % ('127.0.0.1:8000' ,self.get_absolute_url)
 
+    @property
+    def increase_view(self):
+        self.no_of_views += 1
+        self.save()
 
     # def get_project_absolute_url(slug, request):
     #     abs_url = request.build_absolute_uri(reverse('blogs:sub_topic', args = [slug]))
     #     return abs_url
+
+    @classmethod
+    def getAllChapters(cls, slug):
+        project = cls.objects.get(slug = slug)
+        return project.chapters.all()
+
+    @classmethod
+    def get_all_projects(cls):
+        return cls.objects.all()
+
+    
+    @classmethod
+    def get_popular_approved_projects(KClass):
+        return KClass.objects.filter(isApproved = True).order_by('-no_of_views')[:5]
+
+    @classmethod
+    def get_popular_projects(KClass):
+        return KClass.objects.all().order_by('-no_of_views')[:5]
+    
+    @classmethod
+    def getProjectSearches(cls, search_input):
+        project_searches = cls.objects.filter(
+            Q(slug__istartswith = search_input) |
+            Q(title__icontains = search_input) |
+            Q(author__user__username__icontains = search_input)
+        )
+        return project_searches
+
+    @classmethod
+    def get_project(cls, pk):
+        return cls.objects.get(pk = pk)
 
     @classmethod
     def get_status(cls, pk):
@@ -165,61 +203,26 @@ class Project(models.Model):
         return None
 
     @classmethod
-    def getProjectSearches(cls, search_input):
-        project_searches = cls.objects.filter(
-            Q(slug__istartswith = search_input) |
-            Q(title__icontains = search_input) |
-            Q(author__user__username__icontains = search_input)
-        )
-        return project_searches
-
-    @classmethod
     def getTitle(cls, slug):
         project = cls.objects.get(slug = slug)
         return project.title
 
-    @classmethod
-    def getAllChapters(cls, slug):
-        project = cls.objects.get(slug = slug)
-        return project.chapters.all()
-
-    @property
-    def increase_view(self):
-        self.no_of_views += 1
-        self.save()
-
-    @classmethod
-    def get_popular_projects(KClass):
-        return KClass.objects.all().order_by('-no_of_views')[:5]
-
-    @classmethod
-    def get_all_projects(cls):
-        return cls.objects.all()
     
-    @classmethod
-    def get_project(cls, pk):
-        return cls.objects.get(pk = pk)
+    def canUserView(self, user):
+        return self.isApproved or user.is_superuser
 
-    @classmethod
-    def get_popular_approved_projects(KClass):
-        return KClass.objects.filter(isApproved = True).order_by('-no_of_views')[:5]
-
-    @property
-    def complete_project(self):
-        self.isCompleted = True
-        self.save()
 
 class Chapter(models.Model):
-    link_to = models.ForeignKey(Project,on_delete=models.CASCADE, related_name="chapters")
     author = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True, blank=True)
-    date_posted = models.DateTimeField(auto_now_add=True)
-    heading = models.CharField(max_length=40,null=False,blank=False)
-   # content = models.CharField(max_length=1000, null=False,blank=False)
-    description = models.CharField(max_length=300,blank=True,null=True)
-  #  content = HTMLField()  .
     content = RichTextUploadingField(blank=True,null=True)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=300,blank=True,null=True)
+    heading = models.CharField(max_length=40,null=False,blank=False)
+    link_to = models.ForeignKey(Project,on_delete=models.CASCADE, related_name="chapters")
     slug = models.SlugField(null=True,blank=True)
     youtube_link = models.URLField(max_length=200, blank=True, null=True)
+   # content = models.CharField(max_length=1000, null=False,blank=False)
+  #  content = HTMLField()  .
     # no_of_likes = models.IntegerField(default=0)
 
     class Meta:
@@ -238,22 +241,6 @@ class Chapter(models.Model):
             return "http://%s%s" % ('127.0.0.1:8000' ,self.get_absolute_url)
         else:
             return "https://%s%s" % ('127.0.0.1:8000' ,self.get_absolute_url)
-
-
-    # def get_chapter_absolute_url(project_slug, chapter_slug, request):
-    #     abs_url = request.build_absolute_uri(reverse('blogs:chapter_post', args = [project_slug, chapter_slug]))
-    #     return abs_url
-
-    def get_like_url(self):
-        return reverse("api:like-post")
-
-    def get_comment_url(self):
-        return reverse("api:comment-post")
-
-    @classmethod
-    def getChapter(cls, slug):
-        chapter = cls.objects.get(slug = slug)
-        return chapter
 
     @property
     def get_next_chapter(self):
@@ -289,13 +276,30 @@ class Chapter(models.Model):
     @property
     def like_count(self):
         return self.likes.all().count()
+    
 
+    @classmethod
+    def getChapter(cls, slug):
+        chapter = cls.objects.get(slug = slug)
+        return chapter
+
+
+    @classmethod
+    def getAllComments(cls, chapter_content_slug):
+        chapter = cls.objects.get(slug = chapter_content_slug)
+        return chapter.chapter_comments.all().order_by('-timestamp')
+    
+    @classmethod
+    def getChapterSearches(cls, search_input):
+        author_searches = cls.objects.filter(
+            Q(slug__istartswith = search_input) |
+            Q(author__user__username__icontains = search_input) |
+            Q(heading__icontains = search_input)
+        )
+        return author_searches
+    
     def comments(self):
         return self.chapter_comments.all()
-
-    def increaseLikes(self):
-        self.no_of_likes += 1
-        self.save()
 
     def decreaseLikes(self):
         self.no_of_likes -= 1
@@ -314,19 +318,15 @@ class Chapter(models.Model):
         else:
             return None
 
-    @classmethod
-    def getAllComments(cls, chapter_content_slug):
-        chapter = cls.objects.get(slug = chapter_content_slug)
-        return chapter.chapter_comments.all().order_by('-timestamp')
-    
-    @classmethod
-    def getChapterSearches(cls, search_input):
-        author_searches = cls.objects.filter(
-            Q(slug__istartswith = search_input) |
-            Q(author__user__username__icontains = search_input) |
-            Q(heading__icontains = search_input)
-        )
-        return author_searches
+    def increaseLikes(self):
+        self.no_of_likes += 1
+        self.save()
+            
+    def get_like_url(self):
+        return reverse("api:like-post")
+
+    def get_comment_url(self):
+        return reverse("api:comment-post")
 
 
 class Like(models.Model):
@@ -335,8 +335,6 @@ class Like(models.Model):
 
     # def __str__(self):
     #     return str(self.link_to)
-
-
 
 class Comment(models.Model):
     link_to = models.ForeignKey(Chapter, on_delete=models.CASCADE, related_name="chapter_comments")
