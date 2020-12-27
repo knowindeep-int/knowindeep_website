1768db38a1dfb74e78dbd9fa029d441f5598f5e8
Vedant Tripathi <vedant.tripathi02@gmail.com>
2020-12-27 17:42:48 +0530

    added url security for blog post and chapter post pages

--- blogs/views.py
+++ blogs/views.py
@@ -24,39 +24,43 @@ def topics(request):
 
 def subtopics(request,slug):
     project = Project.objects.get(slug=slug)
-    #blog = Chapter.objects.filter(link_to__slug=slug)
-    chapters = Project.getAllChapters(slug)
-    context = {
-        "chapters": chapters,
-        "chapter_heading": slug,
-        "project":project,
-    }
-    return render(request,"blogs/subtopic.html",context)
+    if project.isApproved is True:
+        #blog = Chapter.objects.filter(link_to__slug=slug)
+        chapters = Project.getAllChapters(slug)
+        context = {
+            "chapters": chapters,
+            "chapter_heading": slug,
+            "project":project,
+        }
+        return render(request,"blogs/subtopic.html",context)
+    return HttpResponse('You are not allowed to access this page', status =500)
 
 def chapter_post(request,slug, chapter):
-    chapter_content = Chapter.objects.get(slug=chapter)
-    #all_blogs = Chapter.objects.filter(link_to__slug=slug)
-    chapters = Project.getAllChapters(slug)
-    #main_blog = Project.objects.get(slug=slug)
-    title = Project.getTitle(slug = slug)
-    author = chapter_content.author
-    # main_blog.increase_view
-    # soup = BeautifulSoup(blog_content.content,"lxml")
-    # for heading in soup.find_all(["h1", "h2", "h3"]):
-    #     print(heading.name + ' ' + heading.text.strip())
-    #     print(heading)
-    # print(str(blog_content.get_next_blog) + str(blog_content.get_previous_blog))
-    has_liked = chapter_content.has_user_liked(request.user)
-    context = {
-        "title":title,
-        "chapter_content" : chapter_content,
-        "slug":slug,
-        "chapters":chapters,
-        "author": author,
-        "has_liked": has_liked
-    }
-    return render(request,"blogs/blog_post.html", context)
-
+    project = Project.objects.get(slug = slug)
+    if project.isApproved is True:
+        chapter_content = Chapter.objects.get(slug=chapter)
+        #all_blogs = Chapter.objects.filter(link_to__slug=slug)
+        chapters = Project.getAllChapters(slug)
+        #main_blog = Project.objects.get(slug=slug)
+        title = Project.getTitle(slug = slug)
+        author = chapter_content.author
+        # main_blog.increase_view
+        # soup = BeautifulSoup(blog_content.content,"lxml")
+        # for heading in soup.find_all(["h1", "h2", "h3"]):
+        #     print(heading.name + ' ' + heading.text.strip())
+        #     print(heading)
+        # print(str(blog_content.get_next_blog) + str(blog_content.get_previous_blog))
+        has_liked = chapter_content.has_user_liked(request.user)
+        context = {
+            "title":title,
+            "chapter_content" : chapter_content,
+            "slug":slug,
+            "chapters":chapters,
+            "author": author,
+            "has_liked": has_liked
+        }
+        return render(request,"blogs/blog_post.html", context)
+    return HttpResponse('You are not allowed to access this page', status =500)
 
 def remove(request,slug):
     if request.user.is_superuser:
