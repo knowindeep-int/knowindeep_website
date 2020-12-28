7b73e1b0472f2012e36b39cd2d110e62a5d453ae
Vedant Tripathi <vedant.tripathi02@gmail.com>
2020-12-28 11:16:36 +0530

    created new view to displat all blogs

--- blogs/views.py
+++ blogs/views.py
@@ -80,6 +80,10 @@ def approve(request,slug):
         return redirect('blogs:index')
     return HttpResponse("You are not Authorized to access this Page", status = 500)
 
+def list_all_blogs(request):
+    context = {'projects':Project.objects.all()}
+    return render(request,'blogs/list_all_blogs.html',context)
+
 
 def error404(request, exception):
     return HttpResponse("error404")
