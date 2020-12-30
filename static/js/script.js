121719271fe62ae2de87ac67652caca7f6d9c893
Krishang <krishangsaharia2@gmail.com>
2020-12-31 00:38:56 +0530

    seprated js from base.html

--- static/js/script.js
+++ static/js/script.js
@@ -10,3 +10,73 @@ document.addEventListener('DOMContentLoaded', function() {
 //         window.open("{% url 'social:begin' 'google-oauth2' %}?next={{request.path}}", "", "width=700,height=700");
 //     {% endif %}
 // }
+
+
+/* FOR BASE */ 
+
+function implementSearch() {
+
+    search_input = document.getElementById('searchInput').value;
+    $.ajax({
+        url: '{% url "api:search" %}',
+        method: "GET",
+        data: {
+            'search_input': search_input,
+        },
+        success: function(data) {
+            var result = document.getElementById("result");
+            result.innerHTML = "";
+            result.innerHTML += '<p class="list-group-item" style="color:black;" >Projects:</p>'
+            for (var i = 0; i < data.projects.length; i++) {
+                result.innerHTML += '<a class="list-group-item" href="/' + data.projects[i].slug + '" >' + data.projects[i].title + '</a>';
+            }
+
+            result.innerHTML += '<p class="list-group-item" style="color:black;" >Chapters:</p>'
+            for (var i = 0; i < data.chapters.length; i++) {
+                result.innerHTML += '<a class="list-group-item" href="/' + data.chapters[i].project_slug + '/' + data.chapters[i].slug + '" >' + data.chapters[i].heading + '</a>';
+            }
+
+            result.innerHTML += '<p class="list-group-item" style="color:black;" >Authors:</p>'
+            for (var i = 0; i < data.authors.length; i++) {
+                result.innerHTML += '<a class="list-group-item" href="/@' + data.authors[i].username + '" >' + data.authors[i].username + '</a>';
+            }
+
+        },
+        error: function(error) {
+            alert(error)
+        },
+    });
+};
+
+function functionOne() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+window.addEventListener("resize", function() {
+    var a = window.innerWidth;
+    if (a > 993) {
+        var x = document.getElementById("myLinks");
+        x.style.display = "none";
+    }
+});
+
+window.onscroll = function() {
+    myFunction()
+};
+
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    // document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+(function() { var dialog = document.getElementById('loginDialog'); document.getElementById('loginLink').onclick = function() { dialog.style.display = "block"; }; window.onclick = function() { if (event.target == dialog) { dialog.style.display = "none";
+        }; }; document.getElementById('googleLogin').onclick = function() { var url = this.getAttribute('google-href') window.open(url,"_self") }; document.getElementById('closeDialog').onclick = function() { dialog.style.display = "none"; }; document.getElementById('githubLogin').onclick
+        = function() { var url = this.getAttribute('github-href') window.open(url,"_self") }; // document.addEventListener('DOMContentLoaded', function() { // var element = document.querySelectorAll('.sidenav'); // var instances = M.Sidenav.init(element,
+        options);
