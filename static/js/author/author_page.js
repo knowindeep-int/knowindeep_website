5fd6f6903648d930455652f7cd6fa17840ddf65e
Vedant Tripathi <vedant.tripathi02@gmail.com>
2021-01-01 15:02:55 +0530

    removed templating from html and shifted to js

--- static/js/author/author_page.js
+++ static/js/author/author_page.js
@@ -8,9 +8,25 @@ const FIELDS = {
     DP: "dp",
     PHONENUMBER: "phone_number"
 }
+function makeEditable(is_verified) {
+    // alert('hello')
+    alert(is_verified);
+    $(this).attr('disabled', false);
+}
 
-
-
+function disableEdit(is_verified) {
+    //event.preventDefault();
+    // alert('hello')
+    edit_buttons = document.getElementsByClassName("can_hide")
+    alert(is_verified)
+        //alert('HELLO WORLD!')
+    
+    if (is_verified == "False") {
+        for (var i = 0; i < edit_buttons.length; i++) {
+            edit_buttons[i].style.display = "none";
+        }
+    }
+}
 
 function editProfile(field) {
     let value = document.getElementById(field).value
