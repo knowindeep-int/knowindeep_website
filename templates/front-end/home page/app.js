641fbadfa82dea754745d6124c418a761f63ef23
soulslayer007 <sdhruv934@gmail.com>
2021-01-03 12:41:52 +0530

    changes in search button and dropdown

--- templates/front-end/home page/app.js
+++ templates/front-end/home page/app.js
@@ -21,9 +21,29 @@
 // });
 
 
-function mouseOut() {	var a = document.getElementById("srch");
-a.classList.remove("mystyle");
-}
 function mousein() {	var a = document.getElementById("srch");
 a.classList.add("mystyle");
 }
+
+
+function myFunc(){
+    document.getElementById("form").classList.add("form1");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('#navbarDropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var myDropdown = document.getElementById("form");
+    if (myDropdown.classList.contains('form1')) {
+      myDropdown.classList.remove('form1');
+    }
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('mystyle')) {
+        openDropdown.classList.remove('mystyle');
+      }
+    }
+  }
+}
+
