956b8c758e259575c787c8abb901e7b4f0327f30
Vedant Tripathi <vedant.tripathi02@gmail.com>
2021-01-11 02:17:01 +0530

    added new url for new view

--- teach/urls.py
+++ teach/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import add_course, list_of_projects
+from .views import add_course, list_of_projects,text_editor
 
 app_name = "teach"
 
 urlpatterns=[
     path('add/', add_course, name="add_course"),
     path('add/<int:pk>/', add_course, name="add_course"),
-    path('', list_of_projects, name="list_of_projects")
+    path('', list_of_projects, name="list_of_projects"),
+    path('chapter',text_editor,name="chapter")
 ]
