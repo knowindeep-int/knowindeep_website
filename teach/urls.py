4dfbcbdc03905f0f19288dbf9f83a7119c38695d
Krishang <krishangsaharia2@gmail.com>
2021-01-12 01:59:59 +0530

    added url to get incolplete page of chapter

--- teach/urls.py
+++ teach/urls.py
@@ -8,5 +8,6 @@ urlpatterns=[
     path('add/', add_course, name="add_course"),
     path('add/<int:pk>/', add_course, name="add_course"),
     path('', list_of_projects, name="list_of_projects"),
-    path('chapter/<int:pk>',text_editor,name="chapter")
+    path('chapter/<int:pk>',text_editor,name="chapter"),
+    path('chapter/<int:pk>/<int:chapter_pk>',text_editor,name="chapter")
 ]
