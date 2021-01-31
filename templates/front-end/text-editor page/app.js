4a878f2cbfb82e0f07d2dd0a647d0eeb081bf767
soulslayer007 <sdhruv934@gmail.com>
2021-01-31 22:37:05 +0530

    text-editor

---
+++ templates/front-end/text-editor page/app.js
@@ -0,0 +1,25 @@
+function mousein() {	
+    var a = document.getElementById("srch");
+    a.classList.add("mystyle");
+}
+
+function myFunc(){
+    document.getElementById("form").classList.add("form1");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('#navbarDropdown')) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu");
+    var myDropdown = document.getElementById("form");
+    if (myDropdown.classList.contains('form1')) {
+      myDropdown.classList.remove('form1');
+    }
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('mystyle')) {
+        openDropdown.classList.remove('mystyle');
+      }
+    }
+  }
+}
