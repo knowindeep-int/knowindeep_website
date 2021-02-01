87df1cc20de51231fbf952e1a92df73dc7394333
soulslayer007 <sdhruv934@gmail.com>
2021-02-02 00:11:24 +0530

    profile-page correction

--- templates/front-end/profile page/app.js
+++ templates/front-end/profile page/app.js
@@ -24,24 +24,42 @@ window.onclick = function(event) {
   }
 }
 
-var p1 = document.getElementsByClassName("btn-group")
-var p3 = document.getElementsByClassName("cancel")
-var p2 = document.getElementsByClassName("edit")
-var ele = 0;
+// var p1 = document.getElementsByClassName("btn-group")
+// var p3 = document.getElementsByClassName("cancel")
+// var p2 = document.getElementsByClassName("edit")
+// var ele = 0;
 
-function myFunction() {
+// function myFunction() {
+// 	var i = 0;
+// 	for (i = 0; i < p2.length - 1; i++) {
+// 		p2[i].addEventListener("click", function () {
+// 			p1[ele].classList.toggle("second");
+// 			p2[ele].classList.toggle("second");
+// 		});
+// 		p3[i].addEventListener("click", function () {
+// 			p1[ele].classList.toggle("second");
+// 			p2[ele].classList.toggle("second");
+//     });
+//     ele++;
+// 	}
+// }
 
-	var i = 0;
-	for (i = 0; i < p2.length - 1; i++) {
-		p2[i].addEventListener("click", function () {
-			p1[ele].classList.toggle("second");
-			p2[ele].classList.toggle("second");
-		});
-		p3[i].addEventListener("click", function () {
-			p1[ele].classList.toggle("second");
-			p2[ele].classList.toggle("second");
-		});
-	}
+// myFunction();
+
+function reply_click(e) {
+  e.target.classList.toggle("second");
+  var a=e.target.id;
+  var p1 = document.getElementsByClassName("btn-group");
+  var b= a-1;
+  p1[b].classList.toggle("second");
 }
 
-myFunction();
+function reply(e) {
+  var txt = e.target.id;
+  var numb = txt.match(/\d/g);
+  var p1 = document.getElementsByClassName("btn-group");
+  p1[numb-1].classList.toggle("second");
+  var b=String(numb-1);
+  p3=document.getElementById("b");
+  p3.classList.toggle("second");
+}
