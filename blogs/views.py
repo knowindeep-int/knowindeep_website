f0a2f00a0aa5b49f2707ee13991bb17ed175463a
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-03-12 02:25:55 +0530

    changed file names

--- blogs/views.py
+++ blogs/views.py
@@ -35,7 +35,7 @@ def subtopics(request,slug):
             "project":project,
             "has_liked": has_liked
         }
-        return render(request,"new/blogs/new_subtopic.html",context)
+        return render(request,"new/blogs/new_project_page.html",context)
     return HttpResponse('You are not allowed to access this page', status =500)
 
 def chapter_post(request,slug, chapter):
@@ -62,7 +62,7 @@ def chapter_post(request,slug, chapter):
             "author": author,
             # "has_liked": has_liked
         }
-        return render(request,"new/blogs/new_blog_post.html", context)
+        return render(request,"new/blogs/new_chapter_page.html", context)
     return HttpResponse('You are not allowed to access this page', status =500)
 
 def remove(request,slug):
