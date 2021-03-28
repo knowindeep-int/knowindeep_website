136c280fcead6ca7e6762c4159c8d2d70195164d
Priyansh-Kedia <kediapriyansh@gmail.com>
2021-03-28 14:57:53 +0530

    corrected username in profile

--- blogs/utils.py
+++ blogs/utils.py
@@ -35,8 +35,7 @@ def unique_slug_generator(instance,new_slug=None):
 
 def save_user(backend, user, response, *args, **kwargs):
 
-    user.username = response['name']
-    user.save()
+   
     try:
         profile = models.Profile.objects.get(user=user)
     except models.Profile.DoesNotExist:
@@ -45,10 +44,10 @@ def save_user(backend, user, response, *args, **kwargs):
             dp = response['picture']
         else:
             dp = response['avatar_url']    
-        
-        print(backend)
-        print(user) 
-        print(response)
+
+        user.username = response['name']
+        user.save()
+    
         profile = models.Profile(
             dp = dp,
             user=user
