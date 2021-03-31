9d071a8078406ebed711f0efa012eceef90f0b12
Vedant Tripathi <vedant.tripathi02@gmail.com>
2021-03-31 23:42:06 +0530

    added models

--- blogs/models.py
+++ blogs/models.py
@@ -103,8 +103,9 @@ class Blog(models.Model):
     author = models.ForeignKey(to =Profile, on_delete = models.CASCADE)
     content = RichTextUploadingField(blank=True,null=True)
     date_approved = models.DateTimeField(default = None, blank =True ,null=True) 
-    description = models.TextField(blank=True,null=True)
+    # description = models.TextField(blank=True,null=True)
     isApproved = models.BooleanField(default=False)
+    no_of_views = models.IntegerField(default=0)
     title = models.CharField(max_length = 25, null =False, blank= False)
 
     def save(self, *args, **kwargs):
@@ -114,6 +115,21 @@ class Blog(models.Model):
             self.date_approved = None
         super(Blog,self).save(*args,*kwargs)
     
+class Topic(models.Model):
+    author = models.ForeignKey(to =Profile, on_delete = models.CASCADE)
+    content = RichTextUploadingField(blank=True,null=True)
+    title = models.CharField(max_length = 25, null =False, blank= False)
+    blog = models.ForeignKey(to = Blog, on_delete = models.CASCADE) 
+
+
+class SubTopic(models.Model):
+    author = models.ForeignKey(to =Profile, on_delete = models.CASCADE)
+    content = RichTextUploadingField(blank=True,null=True)
+    title = models.CharField(max_length = 25, null =False, blank= False)
+    topic = models.ForeignKey(to = Topic, on_delete = models.CASCADE)
+
+
+
 
 # class PreRequisite(models.Model):
 #     name = models.CharField(max_length = 100)
