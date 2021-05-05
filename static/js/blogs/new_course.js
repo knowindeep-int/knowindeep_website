3afab5cd461d5660a6936e29a7bddea729ae2fcc
Vedant Tripathi <vedant.tripathi02@gmail.com>
2021-05-05 21:19:09 +0530

    minor frontend change

--- static/js/blogs/new_course.js
+++ static/js/blogs/new_course.js
@@ -415,7 +415,9 @@ setTimeout(function () {
 
  div = document.getElementById("10");
 
- div.innerHTML += `<label for="cat-select">Choose a pet:</label>
+ div.innerHTML += `
+ <div class="container container-fluid container-fluid1"><img src="/media/images/icon.png" class="icon1">
+ <h3">Choose a category:</h3>
 
  <select id="cat-select">
      <option value="">--Please choose an option--</option>
